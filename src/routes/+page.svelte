00a3fd32f43335b8d72321dde5203970e1f52f6d
Yu <echo.aimless@gmail.com>
2026-07-28 11:52:04 +0800

    chore: reset worktree

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import ObservationScene from '$lib/observation-scene.svelte'
-  import StationContent from '$lib/station-content.svelte'
-</script>
-
-<ObservationScene />
-<StationContent />
--- src/shims.d.ts
+++
@@ -1,6 +0,0 @@
-import type { AttributifyAttributes } from '@unocss/preset-attributify'
-
-declare namespace svelteHTML {
-  // extended for unocss attributify preset
-  interface HTMLAttributes extends AttributifyAttributes {}
-}
