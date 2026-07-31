120b948979bd085e723e19a40cd3c2e405d66d3f
Yu <echo.aimless@gmail.com>
2026-07-31 10:30:20 +0800

    docs: update comment note

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,5 +2,6 @@ import type { AttributifyAttributes } from '@unocss/preset-attributify'
 
 declare namespace svelteHTML {
   // extended for unocss attributify preset
+  // suspect to have issue in svelte lsp upstream
   interface HTMLAttributes extends AttributifyAttributes {}
 }
